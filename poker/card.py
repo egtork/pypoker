022bd8122254d8f1dd222b8ceb14abe85a1d49f4
egtork <egtork@gmail.com>
2019-09-02 07:57:03 -0700

    Use rank class

--- poker/card.py
+++ poker/card.py
@@ -1,24 +1,6 @@
 from enum import Enum, IntEnum
 
 
-class Suit(Enum):
-    CLUBS = "c"
-    DIAMONDS = "d"
-    HEARTS = "h"
-    SPADES = "s"
-
-    def __repr__(self):
-        return self.value
-
-
-str_to_suit = {
-    "c": Suit.CLUBS,
-    "d": Suit.DIAMONDS,
-    "h": Suit.HEARTS,
-    "s": Suit.SPADES,
-}
-
-
 class Rank(IntEnum):
     TWO = 2
     THREE = 3
@@ -34,46 +16,62 @@ class Rank(IntEnum):
     KING = 13
     ACE = 14
 
+    def __repr__(self):
+        if self.value < 10:
+            return str(self.value)
+        else:
+            return self.name[0]
 
-rank_to_str = [
-    None,
-    None,
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "T",
-    "J",
-    "Q",
-    "K",
-    "A",
-]
+    def __str__(self):
+        if self.value < 10:
+            return str(self.value)
+        else:
+            return self.name[0]
+
 
 str_to_rank = {
-    "2": 2,
-    "3": 3,
-    "4": 4,
-    "5": 5,
-    "6": 6,
-    "7": 7,
-    "8": 8,
-    "9": 9,
-    "T": 10,
-    "J": 11,
-    "Q": 12,
-    "K": 13,
-    "A": 14,
+    "2": Rank.TWO,
+    "3": Rank.THREE,
+    "4": Rank.FOUR,
+    "5": Rank.FIVE,
+    "6": Rank.SIX,
+    "7": Rank.SEVEN,
+    "8": Rank.EIGHT,
+    "9": Rank.NINE,
+    "T": Rank.TEN,
+    "J": Rank.JACK,
+    "Q": Rank.QUEEN,
+    "K": Rank.KING,
+    "A": Rank.ACE,
+}
+
+
+class Suit(Enum):
+    CLUBS = "c"
+    DIAMONDS = "d"
+    HEARTS = "h"
+    SPADES = "s"
+
+    def __repr__(self):
+        return self.value
+
+    def __str__(self):
+        return self.value
+
+
+str_to_suit = {
+    "c": Suit.CLUBS,
+    "d": Suit.DIAMONDS,
+    "h": Suit.HEARTS,
+    "s": Suit.SPADES,
 }
 
 
 class Card:
     def __init__(self, s: str):
         assert len(s) == 2
-        assert s[1] in ("c", "d", "h", "s")
+        assert s[1] in (Suit.CLUBS.value, Suit.DIAMONDS.value,
+                        Suit.HEARTS.value, Suit.SPADES.value)
         self.rank = str_to_rank[s[0]]
         self.suit = str_to_suit[s[1]]
 
@@ -96,4 +94,4 @@ class Card:
         return self.rank <= other.rank
 
     def __repr__(self):
-        return rank_to_str[self.rank] + self.suit.value
+        return str(self.rank) + str(self.suit)
